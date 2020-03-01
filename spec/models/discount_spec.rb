05ae9243ec26dff2da274f4a62f4012013728432
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-01 15:22:41 -0700

    Add initial bundle and tests for new models

---
+++ spec/models/discount_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Discount, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :item_threshold }
+    it {should validate_presence_of :percentage_off }
+  end
+
+  describe 'relationships' do
+    it { should belong_to :merchant }
+  end
+end
