49cce151aa33e12c9ac2703c903e01b908213be0
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-01 16:49:47 -0700

    Add function for creating a new discount

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -5,4 +5,26 @@ class Merchant::DiscountsController < Merchant::BaseController
 
   def new
   end
+
+  def create
+    merchant = current_user.merchant
+    discount = merchant.discounts.create(discount_params)
+
+    if discount.save
+      redirect_to merchant_discounts_path
+    else
+      generate_flash(discount)
+      render :new
+    end
+  end
+
+  private
+
+  def discount_params
+    params.permit(
+      :name,
+      :item_threshold,
+      :percentage_off
+    )
+  end
 end
