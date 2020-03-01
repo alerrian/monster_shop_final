62302166601e61b1cb0f13bcbc40bcee9d4e39bc
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-01 16:11:34 -0700

    Add test and initial function for creating a discount

---
+++ spec/features/merchant/discounts/create_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'As a merchant employee' do
+  before :each do
+    @merchant_1 = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+    @merchant_2 = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+    @m_user = @merchant_1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword', role: 1)
+    @ogre = @merchant_1.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20.25, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5)
+
+    @discount_1 = @merchant_1.discounts.create!(
+      name: 'Small Discount',
+      item_threshold: 20,
+      percentage_off: 5
+    )
+
+    @discount_2 = @merchant_1.discounts.create!(
+      name: 'Big Discount',
+      item_threshold: 40,
+      percentage_off: 10
+    )
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
+  end
+
+  describe "When I try to create a discount" do
+    it 'can create a new discount' do
+      visit '/merchant/discounts'
+
+      click_on 'Add new Discount'
+
+      expect(current_path).to eq('/merchant/discounts/new') 
+    end
+  end
+end
